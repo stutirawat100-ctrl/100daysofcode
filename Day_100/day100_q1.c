ec1feb24ff31d7ee5447b0d16b1d236df203a7a0
stutirawat100-ctrl <stutirawat100@gmail.com>
2025-11-28 18:52:29 +0530

    Create day100_q1.c

---
+++ Day_100/day100_q1.c
@@ -0,0 +1,33 @@
+/* Q150 Use pointer to struct to modify and display data using -> operator.*/
+
+#include <stdio.h>
+
+// Define Student structure
+struct Student 
+{
+    char name[50];
+    int roll_no;
+    int marks;
+};
+
+int main() 
+{
+    struct Student s;              // Normal structure variable
+    struct Student *ptr = &s;      // Pointer to structure
+
+    // Modify values using pointer and -> operator
+    printf("Enter Name: ");
+    scanf("%s", ptr->name);
+
+    printf("Enter Roll: ");
+    scanf("%d", &ptr->roll_no);
+
+    printf("Enter Marks: ");
+    scanf("%d", &ptr->marks);
+
+    // Display modified data using pointer
+    printf("\nModified Data: Name: %s | Roll: %d | Marks: %d\n",
+           ptr->name, ptr->roll_no, ptr->marks);
+
+    return 0;
+}
